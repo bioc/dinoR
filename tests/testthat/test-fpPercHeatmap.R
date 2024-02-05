9d287be9f274690b40cc11d953c313bde4e57a65
xxxmichixxx <michaela.c.schwaiger@gmail.com>
2024-02-05 09:25:44 +0100

    removed tests to speed up checking

--- tests/testthat/test-fpPercHeatmap.R
+++
@@ -1,9 +0,0 @@
-
-NomeData <- createExampleData()
-NomeData <- footprintCalc(NomeData)
-NomeData <- footprintQuant(NomeData)
-fp <- footprintPerc(NomeData)
-
-test_that("fpPercHeatmap works", {
-  expect_no_error(fpPercHeatmap(fp))
-})
